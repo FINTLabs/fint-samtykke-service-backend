f1c2b7bfcd9c1188db6d027a55aacf5e5fa66ff1
Erling Jahr <erling.jahr@gmail.com>
2022-04-28 08:55:39 +0200

    added config class

--- src/main/java/no/fintlabs/configuration/FintEndpointConfiguration.java
+++ src/main/java/no/fintlabs/configuration/FintEndpointConfiguration.java
@@ -1,22 +1,28 @@
 package no.fintlabs.configuration;
 
-import lombok.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import lombok.Setter;
 import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 
 @Getter
+@Setter
 @Configuration
+@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "concent-config.endpoints.fint-uri")
 public class FintEndpointConfiguration {
 
         private String baseUri;
         private String employeeUri;
-
-
-
-
-
-
+        private String employeessnUri;
+        private String studentUri;
+        private String studentssnUri;
+        private String consentUri;
+        private String processorUri;
+        private String processingUri;
+        private String processingbaseUri;
+        private String personaldataUri;
 
 }
--- src/main/java/no/fintlabs/service/PersonService.java
+++ src/main/java/no/fintlabs/service/PersonService.java
@@ -2,6 +2,7 @@ package no.fintlabs.service;
 
 import no.fint.model.resource.felles.PersonResource;
 import no.fintlabs.client.FintClient;
+import no.fintlabs.configuration.FintEndpointConfiguration;
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +13,18 @@ import java.util.concurrent.ExecutionException;
 public class PersonService {
 
     private final FintClient fintClient;
+    private final FintEndpointConfiguration fintEndpointConfiguration;
 
 
-    public PersonService(FintClient fintClient) {
+    public PersonService(FintClient fintClient, FintEndpointConfiguration fintEndpointConfiguration) {
         this.fintClient = fintClient;
+        this.fintEndpointConfiguration = fintEndpointConfiguration;
     }
 
 
     public PersonResource getPerson(FintJwtEndUserPrincipal principal) throws ExecutionException, InterruptedException {
         PersonResource person = fintClient
-                .getResource("/administrasjon/personal/personalressurs/ansattnummer/509545", PersonResource.class)
+                .getResource(fintEndpointConfiguration.getEmployeeUri()+"509545", PersonResource.class)
                 .toFuture().get();
 
         return person;
