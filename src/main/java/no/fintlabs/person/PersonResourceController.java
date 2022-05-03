64648c351fdd12a649563a51f1864b8db76c55e3
Erling Jahr <erling.jahr@gmail.com>
2022-05-03 16:40:02 +0200

    added service for person object

--- src/main/java/no/fintlabs/person/PersonResourceController.java
+++ src/main/java/no/fintlabs/person/PersonResourceController.java
@@ -2,6 +2,7 @@ package no.fintlabs.person;
 
 
 import lombok.extern.slf4j.Slf4j;
+import no.fint.model.felles.Person;
 import no.fint.model.resource.felles.PersonResource;
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,16 +16,21 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 @RestController
 @RequestMapping("/api/person")
-public class PersonController {
+public class PersonResourceController {
 
     private final PersonService personService;
 
-    public PersonController(PersonService personService) {
+    public PersonResourceController(PersonService personService) {
         this.personService = personService;
     }
 
+    @GetMapping("/personresource")
+    public PersonResource getPersonResource(@AuthenticationPrincipal Jwt jwt) throws ExecutionException, InterruptedException {
+        return personService.getPersonResource(FintJwtEndUserPrincipal.from(jwt));
+    }
+
     @GetMapping
-    public PersonResource getPerson(@AuthenticationPrincipal Jwt jwt) throws ExecutionException, InterruptedException {
+    public Person getPerson(@AuthenticationPrincipal Jwt jwt) throws ExecutionException, InterruptedException {
         return personService.getPerson(FintJwtEndUserPrincipal.from(jwt));
     }
 }
--- src/main/java/no/fintlabs/person/PersonService.java
+++ src/main/java/no/fintlabs/person/PersonService.java
@@ -1,14 +1,20 @@
 package no.fintlabs.person;
 
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.felles.Person;
+import no.fint.model.resource.Link;
 import no.fint.model.resource.felles.PersonResource;
 import no.fintlabs.fint.FintClient;
 import no.fintlabs.fint.FintEndpointConfiguration;
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-
+@Slf4j
 @Component
 public class PersonService {
 
@@ -20,8 +26,20 @@ public class PersonService {
         this.fintEndpointConfiguration = fintEndpointConfiguration;
     }
 
-    public PersonResource getPerson(FintJwtEndUserPrincipal principal) throws ExecutionException, InterruptedException {
+    public Person getPerson(FintJwtEndUserPrincipal principal) throws ExecutionException, InterruptedException {
+        PersonResource personResource = fintClient.getResource(
+                fintEndpointConfiguration.getEmployeeUri() + principal.getEmployeeId(),PersonResource.class)
+                .toFuture().get();
+        Map<String,List<Link>> personResourceLinks = personResource.getLinks();
+        List<Link> personLinks = personResourceLinks.get("person");
+        String personLink = personLinks.get(0).toString();
 
+        Person person = fintClient.getResource(personLink,Person.class)
+                .toFuture().get();
+        return person;
+    }
+
+    public PersonResource getPersonResource(FintJwtEndUserPrincipal principal) throws ExecutionException, InterruptedException {
         return fintClient
                 .getResource(
                         fintEndpointConfiguration.getEmployeeUri() + principal.getEmployeeId(),
