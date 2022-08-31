ed077307b80213bc8b2bb02aceb9859dd96d925a
Erling Jahr <erling.jahr@gmail.com>
2022-08-31 15:23:33 +0200

    cleaning. removed unused imports and dep

--- src/main/java/no/fintlabs/fint/FintClient.java
+++ src/main/java/no/fintlabs/fint/FintClient.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.retry.Repeat;
 
