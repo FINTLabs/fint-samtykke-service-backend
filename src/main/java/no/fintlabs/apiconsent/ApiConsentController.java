824b512e390c2a1c04db1647990b4a14ffa9895c
Erling Jahr <erling.jahr@vigoiks.no>
2023-06-05 21:38:20 +0200

    lagt til config for å kunne kjøre app lokal

--- src/main/java/no/fintlabs/apiconsent/ApiConsentController.java
+++ src/main/java/no/fintlabs/apiconsent/ApiConsentController.java
@@ -15,7 +15,7 @@ import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RestController
-@RequestMapping()
+@RequestMapping
 public class ApiConsentController {
     private final ApiConsentService apiConsentService;
 
